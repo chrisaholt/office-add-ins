27e1b09a887527b61c275ca373d2212e6adc3d73
Chris Aholt <35589544+chrisaholt@users.noreply.github.com>
2024-02-19 08:03:25 -0800

    Adds Hello World-type functionality

--- Hello-World Office Add-in/src/taskpane/taskpane.js
+++ Hello-World Office Add-in/src/taskpane/taskpane.js
@@ -14,7 +14,26 @@ Office.onReady((info) => {
 });
 
 export async function run() {
-  /**
-   * Insert your OneNote code here
-   */
-}
+  try {
+    await OneNote.run(async (context) => {
+
+        // Get the current page.
+        const page = context.application.getActivePage();
+
+        // Queue a command to set the page title.
+        page.title = "Hello World";
+
+        // Queue a command to add an outline to the page.
+        var html = "This page was created by following ";
+        html = html.concat(
+          "<a href=\"https://learn.microsoft.com/en-us/office/dev/add-ins/quickstarts/onenote-quickstart\">OneNote Add-in Quickstart</a>"
+        );
+        html = html.concat("<p><ol><li>Item #1</li><li>Item #2</li></ol></p>");
+        page.addOutline(40, 90, html);
+
+        // Run the queued commands.
+        await context.sync();
+    });
+} catch (error) {
+    console.log("Error: " + error);
+}}
